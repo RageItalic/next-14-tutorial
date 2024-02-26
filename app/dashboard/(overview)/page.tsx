fcef48801bb455e729269a57e780210e010c5b81
Parth Patel <parthpatelgee@gmail.com>
2024-02-26 17:17:11 -0500

    built out CRUD for invoices

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,38 @@
+import { Suspense } from "react";
+import { /*fetchCardData, fetchLatestInvoices, fetchRevenue */ } from "../../lib/data";
+import CardWrapper, { Card } from "../../ui/dashboard/cards";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import { lusitana } from "../../ui/fonts";
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
+
+export default async function Page() {
+    // const revenue = await fetchRevenue()
+    // const latestInvoices = await fetchLatestInvoices()
+    // const cardData = await fetchCardData()
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                {/* <Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
+                <Card title="Pending" value={cardData.totalPendingInvoices} type="pending" />
+                <Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" />
+                <Card title="Total Customers" value={cardData.numberOfCustomers} type="customers" /> */}
+
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    )
+}
---
+++ app/lib/actions.ts
@@ -0,0 +1,109 @@
+'use server'
+
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod'
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data
+    const amtInCents = amount * 100
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+        await sql`
+        insert into invoices (customer_id, amount, status, date)
+        values (${customerId}, ${amtInCents}, ${status}, ${date}) 
+        `
+    } catch (e) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data
+    const amtInCents = amount * 100
+
+    try {
+        await sql`
+    update invoices
+    set customer_id = ${customerId}, amount = ${amtInCents}, status = ${status}
+    where id = ${id}
+    `
+    } catch (e) {
+        return {
+            message: 'Database Error: Failed to update Invoice.',
+        };
+    }
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string, formData: FormData) {
+    try {
+        await sql`
+        delete from invoices where id = ${id}
+        `
+        revalidatePath('/dashboard/invoices')
+        return { message: 'Deleted invoice.' }
+    } catch (e) {
+        return {
+            message: 'Database Error: Failed to delete Invoice.',
+        };
+    }
+
+}
